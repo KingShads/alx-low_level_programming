563ef2561ac932e8521366fe3b0070b76f15843d
KingShads <shadrack808@gmal.com>
2023-07-13 18:24:21 -0700

    with new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  *main - this function with proper grammar, but the outcome is a piece of art,
  *
- *Return: 0 when successful 
+ *Return: 0 when successful
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,|n");
 	return (0);
 }
