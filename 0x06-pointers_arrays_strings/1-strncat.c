111f5550531b3e8d3b6d9734a0fc84303d237227
KingShads <shadrack808@gmail.com>
2023-07-27 11:37:39 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest_ptr = dest;
+
+    /* Find the end of the destination string */
+	while (*dest != '\0')
+		dest++;
+
+    /* Append the source string to the destination string */
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+    /* Null-terminate the resulting string */
+	*dest = '\0';
+
+	return (dest_ptr);
+}
