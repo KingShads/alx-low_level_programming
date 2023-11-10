ca84fbdeede9638476fdf2936c897f17f8aad50e
KingShads <shadrack808@gmail.com>
2023-11-10 04:12:01 -0800

    task 2 with corrections

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * _strncpy - copy strings
  * @dest: destination string
  * @src: source string
- * n: number of bytes to copy
+ * @n: number of bytes to copy
  * Return: dest
  */
 
@@ -12,12 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = '\0';
-		i++;
+		if (i < (int)strlen(src))
+		{
+			dest[i] = src[i];
+		}
+		if (i >= (int)strlen(src))
+		{
+			dest[i] = '\0';
+		}
 	}
 	return (dest);
 }
