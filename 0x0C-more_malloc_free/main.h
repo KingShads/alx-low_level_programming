80853bb8c5baa4a7c9375dc21a2a27089f2440ba
KingShads <shadrack808@gmail.com>
2023-11-23 14:52:57 -0800

    main second correction

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,5 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <string.h>
-
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int _putchar(char);
-
-#endif /*MAIN_H*/
